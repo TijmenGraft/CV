d26a6115ed26eccc75c7e5f4e9244a4f20a840c7
tijmen van graft <T.R.D.vanGraft@student.tudelft.nl>
2018-02-01 00:41:46 +0100

    Add index.html

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function(req,res) {
 	res.send("hello world");
