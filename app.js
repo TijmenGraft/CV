02eccc25a520192211673ced16ec6b8abfac9933
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2018-02-09 15:00:36 +0100

    Mass commit for sass and other stuff

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
 app.use(
     sass({
-		src: path.join(__dirname, 'public/stylesheets/sass'),
-		dest: path.join(__dirname, 'public/stylesheets'),
+		src: path.join(__dirname, '/sass'),
+		dest: path.join(__dirname, 'client/css'),
 		debug: true,
-		indentedSyntax: true,
-		outputStyle: 'compressed',
-		prefix: '/stylesheets'
+		indentedSyntax: false,
+		prefix: '/css'
 	})		
 );
 
@@ -29,7 +29,7 @@ app.get('/', function(req,res) {
 	res.send("hello world");
 })
 
-var server = app.listen(process.env.PORT, function() {
+var server = app.listen('80', function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log("Server is listening on %s:%s", host, port);
