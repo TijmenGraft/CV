2052d693ce4987ec42c7883b811c6618b959323a
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2018-02-18 19:34:28 +0100

    more menu fixing

--- client/js/navigation.js
+++ client/js/navigation.js
@@ -6,26 +6,3 @@ $(document).ready(function() {
 		})
 	});
 });
-
-SELECT *
-FROM (
-  SELECT *
-  FROM (
-    SELECT *
-    FROM yourtable a
-    WHERE (
-      SELECT COUNT(*)
-      FROM yourtable
-      WHERE (parentid = a.parentid AND parentid IS NOT NULL AND a.parentid IS NOT NULL)
-      AND creationdate <= a.creationdate
-    ) <= 2 AND a.parentid IS NOT NULL
-    UNION 
-    SELECT * 
-    FROM (
-        SELECT *
-        FROM yourtable
-        WHERE parentid IS NULL
-        LIMIT 3
-      ) AS bar
-  )AS foo
-  ORDER BY parentid) AS buz;
