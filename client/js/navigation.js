7ef7b71b1066fc04b857cf16fdbb02aa0e3181e5
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2018-02-19 23:15:22 +0100

    starting with loader

--- client/js/navigation.js
+++ client/js/navigation.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 	$('.navigation-div').on('click', function(e) {
 		e.preventDefault();
-		console.log('test');
 		$.ajax({
-
+			async: true,
+			url: "/category/content",
+			type: "post",
+			dataType: "html",
+			cache: true,
+			data: {
+				token: "testtoken"
+			}
+		}).done(function(data) {
+			console.log('success');
 		})
 	});
 });
--- modules/router.js
+++ modules/router.js
@@ -1,22 +1,42 @@
 function logger(req, res, next) {
 	console.log('We have a new request for: %s \t %s \t %s \t %s', new Date(), req.method, req.url);
-	console.log(req.headers.user-agent);
+	console.log(req.headers);
+	console.log(req.referer);
 	next();
+}
+
+function checkToken(req, res, next) {
+	console.log('currently checking for correct csrf token');
+	console.log(req.body.token);
+	next();
+	// if(req.body.token === "testtoken") {
+	// 	next();
+	// } else {
+	// 	var err = new Error();
+	// 	err.status = 403;
+	// 	res.status(403);
+	// }
 }
 
 module.exports = function(app) {
 	app.use(logger);
+	app.use(checkToken);
 
-	app.get('/', function(req, res, next) {
-		res.render('index.ejs');
+	app.get('/main', function(req, res, next) {
+		console.log('/ route');
+		res.render('index.ejs', {'some data': 'some values'}, function(err,html) {
+			res.send(html)
+		});
 	})
 
 	app.get('/:map', function(req, res,next) {
+		console.log('/:map route');
 		console.log(req.params.map);
 	})
 
-	app.get('/:map/:conent', function(req,res,next) {
-		console.log(req.map);
+	app.get('/:map/:content', function(req,res,next) {
+		console.log('/:map/:content route');
+		console.log(req.params.map);
 		next();
 	});
 }
