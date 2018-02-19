9b71a2d37ec348264dd3016a329b8b483b3c5cbc
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2018-02-19 20:09:20 +0100

    starting with ejs

--- modules/router.js
+++ modules/router.js
@@ -1,12 +1,18 @@
-function logger(res,req,next) {
-	console.log('We have a new request for: %s', req);
+'use strict';
+
+function logger(req,res,next) {
+	console.log('We have a new request for: %s %s %s', new Date(), req.method, req.url);
 	next();
 }
 
 module.exports = function(app) {
 	app.use(logger);
 
-	app.get('/:map/:conent', function(res,req,next) {
+	app.get('/', function(req, res, next) {
+		res.render('index.ejs');
+	})
+
+	app.get('/:map/:conent', function(req,res,next) {
 		console.log(req.map);
 		next();
 	});
