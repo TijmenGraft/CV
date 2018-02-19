88e2e7d6ae5bc8a1222e0ba84529025ef42cb1f1
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2018-02-19 20:59:04 +0100

    some things

--- modules/router.js
+++ modules/router.js
@@ -1,13 +1,18 @@
-function logger(res,req,next) {
-	console.log('We have a new request for: %s', req);
+function logger(req, res, next) {
+	console.log('We have a new request for: %s \t %s \t %s', new Date(), req.method, req.url);
 	next();
 }
 
 module.exports = function(app) {
 	app.use(logger);
 
-	app.get('/:map/:conent', function(res,req,next) {
-		console.log(req.map);
+	app.get('/:map', function(req, res,next) {
+		console.log(req.params.map);
+		next();
+	});
+
+	app.get('/:map/:conent', function(req, res, next) {
+		console.log(req.params.map);
 		next();
 	});
 }
