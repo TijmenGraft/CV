b613560ceb7e559e559efa8617240a187fe242c5
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2018-02-19 21:46:51 +0100

    fixing project setup

--- client/js/navigation.js
+++ client/js/navigation.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('.navigation-div').on('click', function(e) {
 		e.preventDefault();
+		console.log('test');
 		$.ajax({
 
 		})
--- modules/router.js
+++ modules/router.js
@@ -1,24 +1,22 @@
 function logger(req, res, next) {
-	console.log('We have a new request for: %s \t %s \t %s', new Date(), req.method, req.url);
+	console.log('We have a new request for: %s \t %s \t %s \t %s', new Date(), req.method, req.url);
+	console.log(req.headers.user-agent);
 	next();
 }
 
 module.exports = function(app) {
 	app.use(logger);
 
-	app.get('/:map', function(req, res,next) {
-		console.log(req.params.map);
 	app.get('/', function(req, res, next) {
 		res.render('index.ejs');
 	})
 
+	app.get('/:map', function(req, res,next) {
+		console.log(req.params.map);
+	})
+
 	app.get('/:map/:conent', function(req,res,next) {
 		console.log(req.map);
-		next();
-	});
-
-	app.get('/:map/:conent', function(req, res, next) {
-		console.log(req.params.map);
 		next();
 	});
 }
