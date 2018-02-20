63bc8676c9c2184e6234f58ca03790d3a6eb0f87
Unknown <T.R.D.vanGraft@student.tudelft.nl>
2018-02-20 09:02:49 +0100

    add favicon and token accept

--- modules/router.js
+++ modules/router.js
@@ -1,4 +1,5 @@
 function logger(req, res, next) {
+	console.log(req);
 	console.log('We have a new request for: %s \t %s \t %s \t %s', new Date(), req.method, req.url);
 	console.log(req.headers);
 	console.log(req.referer);
@@ -7,22 +8,22 @@ function logger(req, res, next) {
 
 function checkToken(req, res, next) {
 	console.log('currently checking for correct csrf token');
-	console.log(req.body.token);
-	next();
-	// if(req.body.token === "testtoken") {
-	// 	next();
-	// } else {
-	// 	var err = new Error();
-	// 	err.status = 403;
-	// 	res.status(403);
-	// }
+	console.log(req.body);
+	if(req.body.token === "testtoken") {
+		console.log("token accepted");
+		next();
+	} else {
+		var err = new Error();
+		err.status = 403;
+		res.status(403);
+	}
 }
 
 module.exports = function(app) {
 	app.use(logger);
 	app.use(checkToken);
 
-	app.get('/main', function(req, res, next) {
+	app.use('/main', function(req, res, next) {
 		console.log('/ route');
 		res.render('index.ejs', {'some data': 'some values'}, function(err,html) {
 			res.send(html)
